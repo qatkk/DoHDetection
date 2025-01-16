c201003bf5ee7c94c629f04adb729baaaa35e51e
miurelc <miurelangelique@live.com>
2025-01-16 14:08:21 +0100

    pruebas incluyendo modelo Guillem

--- doh_decryptor/packetAnalyzer.py
+++ doh_decryptor/packetAnalyzer.py
@@ -1,4 +1,7 @@
 import statistics
+import joblib
+import pandas as pd
+import ipaddress
 from collections import defaultdict
 from doh_decryptor.context.packet_direction import PacketDirection
 from doh_decryptor.flow_session import FlowManager
@@ -14,9 +17,9 @@ class PacketAnalyzer:
             'bytes_received': 0,
             'timestamps': [],
             'packet_lengths': [],
-            'response_times': []
+            'response_times': [],
         })
-
+        self.model = joblib.load('./knn.joblib')
         with open(self.output_file, 'w') as output:
             output.write("SourceIP,DestinationIP,SourcePort,DestinationPort,TimeStamp,Duration,"
                          "FlowBytesSent,FlowSentRate,FlowBytesReceived,FlowReceivedRate,"
@@ -26,9 +29,9 @@ class PacketAnalyzer:
                          "PacketTimeVariance,PacketTimeStandardDeviation,PacketTimeMean,"
                          "PacketTimeMedian,PacketTimeMode,PacketTimeSkewFromMedian,"
                          "PacketTimeSkewFromMode,PacketTimeCoefficientofVariation,"
-                         "ResponseTimeVariance,ResponseTimeStandardDeviation,ResponseTimeMean,"
-                         "ResponseTimeMedian,ResponseTimeMode,ResponseTimeSkewFromMedian,"
-                         "ResponseTimeSkewFromMode,ResponseTimeCoefficientofVariation,Classification\n")
+                         "ResponseTimeTimeVariance,ResponseTimeTimeStandardDeviation,ResponseTimeTimeMean,"
+                         "ResponseTimeTimeMedian,ResponseTimeTimeMode,ResponseTimeTimeSkewFromMedian,"
+                         "ResponseTimeTimeSkewFromMode,ResponseTimeTimeCoefficientofVariation,Classification\n")
 
     def determine_direction(self, packet):
         forward_key = get_packet_flow_key(packet, PacketDirection.FORWARD)
@@ -46,6 +49,21 @@ class PacketAnalyzer:
         data = self.flow_manager.add_packet_to_flow(packet, direction)
         with open(self.output_file, 'a') as output:
             output.write(','.join(str(data[key]) for key in data.keys()) + '\n')
+        
+        df = pd.DataFrame([data])
+        df= df.drop(columns=['TimeStamp'])
+        df= df.drop(columns=['DoH'])
+        
+        df['SourceIP'] = df['SourceIP'].apply(lambda ip: int(ipaddress.ip_address(ip)))
+        df['DestinationIP'] = df['DestinationIP'].apply(lambda ip: int(ipaddress.ip_address(ip)))
+        
+        df = df.apply(pd.to_numeric, errors='coerce').fillna(0)
+        
+        prediction = (self.model).predict(df)
+        print(f"Prediction for packet is: {prediction}")
+        
+        with open('predictions.txt', 'w') as output:
+            output.write('Prediction for packet:' + str(prediction) + '\n')
 
 
     def process_packets(self, packet_queue):
